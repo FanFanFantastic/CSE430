79d3eaa2ffd129fd62d08bb20a8d898e5112da51
FanFanFantastic <stardustboylong@gmail.com>
2016-02-21 23:52:13 -0700

    Fan's project1 step 1 without memory management

--- my_ps.c
+++ my_ps.c
@@ -13,7 +13,7 @@ struct my_taskList
 	int task_pid;
 	char task_tty[64];
 	unsigned long task_time;
-	char task_comm[15];
+	char task_comm[32];
 
 }my_taskList;
 
@@ -22,13 +22,13 @@ asmlinkage long sys_my_ps(int b, char *c)
 	
 	struct my_taskList kernel_buff[1024];
 	int len = 0;
-	int num = 0;
+	//	int num = 0;
 	unsigned long jiff,time;
 	int i = 0, n = 0;
 			
 	struct task_struct *task;
 	//getting the size of the kernel_buffer
-	num = sizeof(kernel_buff)/sizeof(my_taskList);
+//	num = sizeof(kernel_buff)/sizeof(my_taskList);
 
 	//kernel_buff = kmalloc(1024 * sizeof(my_taskList),GFP_KERNEL);
 
@@ -40,15 +40,22 @@ asmlinkage long sys_my_ps(int b, char *c)
 		thread_group_cputime_adjusted(task,&utime,&stime);
 		jiff = utime+stime;		
 		time  = jiff / HZ;
-		printk("%d\n",task->pid);
+	//	printk("%d\n",task->pid);
 		//pr_info("%s [%d]\n",task->comm,task->pid,task->signl->tty
 			kernel_buff[i].task_time = time;
 		//kernel_buff[i].task_tty = task->signal->tty->name;
-			strcpy(kernel_buff[i].task_tty,task->signal->tty->name);
+			if(task->signal->tty == NULL)
+				strcpy(kernel_buff[i].task_tty,"?");
+			else
+				strcpy(kernel_buff[i].task_tty,task->signal->tty->name);
+		
+
+
 			kernel_buff[i].task_pid = task->pid;
-			//kernel_buff[i].task_comm = task->comm;
+		//	kernel_buff[i].task_comm = task->comm;
 			strcpy(kernel_buff[i].task_comm, task->comm);
 	
+	//	printk("%d\n",task->comm);
 		//memcpy(kernel_buffer,task,sizeof(task));
 
 		len =  len + sizeof(my_taskList);
--- userspaceP2.c
+++ userspaceP2.c
@@ -2,13 +2,13 @@
 #include <linux/sched.h>
 #include <string.h>
 #include <stdio.h>
-#define __NR_my_ps 359
+#define __NR_my_ps 360
 struct my_struct 
 {
 	int task_pid;
 	char task_tty[64];
 	unsigned long task_time;
-	char task_comm[15];	
+	char task_comm[32];	
 }my_struct;
 
 int main()
@@ -16,7 +16,7 @@ int main()
 	int i=0;
 	int num =0; 
 	struct my_struct userbuff[1024];
-
+	unsigned long TIME;
 //	num = sizeof(userbuff)/sizeof(my_struct);
 
 /*
@@ -31,16 +31,19 @@ int main()
 	}
 */
 
-	syscall (__NR_my_ps,sizeof(userbuff),userbuff);
+	num = syscall (__NR_my_ps,sizeof(userbuff),userbuff);
 	
-	printf("PID TTY\tTIME CMD\n");
+//	struct my_struct userbuff[num+1];
+	printf(" PID TTY\t    TIME CMD\n");
 	
 	for(i =0;i<num;i++)
 	{
-        	printf("%d ",userbuff[i].task_pid);
-		printf("%s",userbuff[i].task_tty);
-		printf("%lu\t ",userbuff[i].task_time);
+		TIME = userbuff[i].task_time;
+        	printf(" %4d  ",userbuff[i].task_pid);
+		printf("%s\t",userbuff[i].task_tty);
+		printf("%02lu:%02lu:%02lu ",TIME/(3600),TIME/60%60,TIME%60);
 		printf("%s\n",userbuff[i].task_comm);
+	
 	}
 
 	return 0;
